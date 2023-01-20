c9987db2755a6e02851be76c4b776b69a5d3b4d6
PedroLouback <pcampos952@gmail.com>
2023-01-20 12:01:28 -0300

    Aula 04 - FInalizada

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-17', '01-18', '01-19'],
-    study: ['01-17', '01-19'],
-    water: ['01-17', '01-19'],
+button.addEventListener('click', add)
+form,addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia já incluso")
+        return
+    }
+
+    nlwSetup.addDay(today )
 }
 
-nlwSetup.setData(data)
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
